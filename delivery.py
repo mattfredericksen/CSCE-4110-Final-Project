527490c30bd9f15b88a8faf554ac92fb249a3d93
mattfredericksen <mfred98@gmail.com>
2020-12-05 14:06:07 -0600

    Refactored shared code

---
+++ delivery.py
@@ -0,0 +1,165 @@
+from functools import lru_cache
+from collections import defaultdict
+from heapq import heapify, heappush, heappop
+
+from random import randint, uniform
+from math import log10
+
+import matplotlib.pyplot as plt
+
+
+class Item:
+    """A class representing the item weight and value for a Delivery."""
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+        self.ratio = value / weight
+
+    def __lt__(self, other):
+        """Comparisons are based on value/weight ratio"""
+        return self.ratio < other.ratio
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(value={self.value}, weight={self.weight})"
+
+
+class Address:
+    """A class representing the location of a Delivery."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __sub__(self, other):
+        return (self.x - other.x), (self.y - other.y)
+
+    @lru_cache(maxsize=None)
+    def distance_to(self, other):
+        """Gets the Euclidean distance between two Addresses.
+        The calculation is stored in an unlimited cache, so
+        comparing n=1000 Addresses to each other will consume
+        O(n^2)=1_000_000 space, which isn't very much."""
+        return sum(coord**2 for coord in (self - other)) ** 0.5
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(x={self.x:.3}, y={self.y:.3})"
+
+
+class Delivery:
+    """A class for combining an Item and Address into a Delivery."""
+    def __init__(self, item, address):
+        self.item = item
+        self.address = address
+
+    @staticmethod
+    def generate(n):
+        """Returns `n` randomly generated Deliveries."""
+        # The bounds below are reasonable but arbitrary.
+        return [
+            Delivery(
+                Item(value=randint(5, 200), weight=randint(1, 50)),
+                Address(x=uniform(0, log10(n)), y=uniform(0, log10(n)))
+            ) for _ in range(n)
+        ]
+
+    def __lt__(self, other):
+        return self.item < other.item
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.item}, {self.address})"
+
+
+class DeliveryPath:
+    """This class is used to directly compare the profitability
+    of two sets of deliveries. Its properties are cached, so
+    their values will be stored after the initial calculation.
+    The initial input is transformed into a path through an MST.
+    """
+    def __init__(self, deliveries=(), make_mst=True):
+        self.path = tuple(deliveries)
+        if len(deliveries) > 2 and make_mst:
+            self.create_mst_path()
+
+    @property
+    @lru_cache(maxsize=1)
+    def length(self):
+        return sum(self.path[i].address.distance_to(self.path[i+1].address)
+                   for i in range(len(self.path) - 1))
+
+    @property
+    @lru_cache(maxsize=1)
+    def profit(self):
+        return sum(d.item.value/100 for d in self.path) - 2 * self.length
+
+    @property
+    @lru_cache(maxsize=1)
+    def weight(self):
+        return sum(d.item.weight for d in self.path)
+
+    def create_mst_path(self):
+        """Returns a path through the MST formed from `deliveries`.
+        Adapted from:
+        https://bradfieldcs.com/algos/graphs/prims-spanning-tree-algorithm/
+        """
+        graph = defaultdict(dict)
+
+        # using a simple heuristic here such as picking the left-most
+        # vertex as a starting point yields less than 1% average performance
+        # increase with dense input, but it's fast so why not?
+        starting_vertex = min(self.path, key=lambda d: d.address.x)
+
+        for i, d1 in enumerate(self.path):
+            for j in range(i + 1, len(self.path)):
+                d2 = self.path[j]
+                graph[d1][d2] = d1.address.distance_to(d2.address)
+                graph[d2][d1] = graph[d1][d2]
+
+        visited = {starting_vertex}
+        edges = [
+            (cost, starting_vertex, to)
+            for to, cost in graph[starting_vertex].items()
+        ]
+        heapify(edges)
+
+        self.path = []
+        while edges:
+            cost, frm, to = heappop(edges)
+            if to not in visited:
+                visited.add(to)
+                self.path.append(to)
+                for to_next, cost in graph[to].items():
+                    if to_next not in visited:
+                        heappush(edges, (cost, to, to_next))
+
+    def plot(self, all_deliveries=(), label=False):
+        """Helper function to visualize paths."""
+        all_deliveries = all_deliveries or self.path
+
+        min_x, min_y, max_x, max_y = (*[float('inf')] * 2, *[float('-inf')] * 2)
+        for d in all_deliveries:
+            x, y = d.address
+            plt.plot(x, y, 'ro')
+            if label:
+                plt.annotate(repr(d.item), (x, y))
+            min_x, min_y, max_x, max_y = min(x, min_x), min(y, min_y), max(x, max_x), max(y, max_y)
+
+        plt.xlim(min_x - max_x * 0.1, max_x * 1.1)
+        plt.ylim(min_y - max_y * 0.1, max_y * 1.1)
+        for i in range(len(self.path) - 1):
+            a1, a2 = (d.address for d in self.path[i:i + 2])
+            plt.annotate("", xy=(*a2,), xytext=(*a1,), arrowprops=dict(arrowstyle="->"))
+        plt.show()
+
+    def __iter__(self):
+        return iter(self.path)
+
+    def __getitem__(self, item):
+        return self.path[item]
+
+    def __len__(self):
+        return len(self.path)
+
+    def __lt__(self, other):
+        return (self.profit, -self.weight) < (other.profit, -other.weight)
--- main.py
+++ main.py
@@ -1,142 +1,26 @@
 from itertools import combinations, permutations
-from functools import lru_cache
-from random import randint, uniform
-
-import matplotlib.pyplot as plt
+from delivery import DeliveryPath, Delivery
 
 
-class Item:
-    def __init__(self, value, weight):
-        self.value = value
-        self.weight = weight
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(value={self.value}, weight={self.weight})"
-
-
-class Address:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __sub__(self, other):
-        if not isinstance(other, self.__class__):
-            raise NotImplementedError(f'{other} is not an {self.__class__.__name__}')
-        return (self.x - other.x), (self.y - other.y)
-
-    @lru_cache(maxsize=None)
-    def distance_to(self, other):
-        return sum(coord**2 for coord in (self - other)) ** 0.5
-
-    def __iter__(self):
-        return iter((self.x, self.y))
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(x={self.x:.3}, y={self.y:.3})"
-
-
-class Delivery:
-    def __init__(self, item, address):
-        self.item = item
-        self.address = address
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.item}, {self.address})"
-
-
-class DeliveryPath:
-    def __init__(self, deliveries=()):
-        self.path = deliveries
-
-    def __add__(self, other):
-        """Warning: this algorithm is flawed.
-        A TSP solution cannot be iteratively developed. A new "city"
-        can invalidate the entire previous solution.
-        """
-        if not isinstance(other, Delivery):
-            raise NotImplementedError(f'{other} is not a Delivery')
-        new_path = DeliveryPath([other] + self.path)
-        deliveries = new_path.path
-        shortest_insertion_point = 0
-        # shortest = new_path.length
-        for i in range(0, len(self.path)):
-            deliveries[i], deliveries[i+1] = deliveries[i+1], deliveries[i]
-            # if new_path.length <= shortest:
-            #     shortest_insertion_point = i + 1
-        deliveries.insert(shortest_insertion_point, other)
-        deliveries.pop()
-        return new_path
-
-    @property
-    @lru_cache(maxsize=1)
-    def length(self):
-        return sum(self.path[i].address.distance_to(self.path[i+1].address)
-                   for i in range(len(self.path) - 1))
-
-    @property
-    @lru_cache(maxsize=1)
-    def profit(self):
-        return sum(d.item.value/100 for d in self.path) - 2 * self.length
-
-    @property
-    @lru_cache(maxsize=1)
-    def weight(self):
-        return sum(d.item.weight for d in self.path)
-
-    def __iter__(self):
-        return iter(self.path)
-
-    def __getitem__(self, item):
-        return self.path[item]
-
-    def __len__(self):
-        return len(self.path)
-
-    def __gt__(self, other):
-        if not isinstance(other, self.__class__):
-            raise NotImplementedError(f'{other} is not an {self.__class__.__name__}')
-        return (self.profit, -self.weight) > (other.profit, -other.weight)
-
-
-def plot_path(deliveries, path):
-    min_x, min_y, max_x, max_y = (*[float('inf')]*2, *[float('-inf')]*2)
-    for d in deliveries:
-        x, y = d.address
-        plt.plot(x, y, 'ro')
-        plt.annotate(repr(d.item), (x, y))
-        min_x, min_y, max_x, max_y = min(x, min_x), min(y, min_y), max(x, max_x), max(y, max_y)
-
-    plt.xlim(min_x - max_x*0.1, max_x * 1.1)
-    plt.ylim(min_y - max_y*0.1, max_y * 1.1)
-    for i in range(len(path) - 1):
-        a1, a2 = (d.address for d in path[i:i+2])
-        plt.annotate("", xy=(*a2,), xytext=(*a1,), arrowprops=dict(arrowstyle="->"))
-        # plt.arrow(a1.x, a1.y, *(a2 - a1), head_width=0.5, length_includes_head=True)
-    plt.show()
-
-
-if __name__ == '__main__':
+def main(deliveries):
     W = 250
-    D = []
-    for _ in range(11):
-        item = Item(randint(10, 200), randint(1, 100))
-        address = Address(uniform(0, 1), uniform(0, 1))
-        D.append(Delivery(item, address))
+    D = deliveries
 
     best_path = DeliveryPath()
 
     for count in range(1, len(D)):
         for haul in combinations(D, count):
-            shortest_path = DeliveryPath(haul)
+            shortest_path = DeliveryPath(haul, make_mst=False)
 
             if shortest_path.weight > W:
                 continue
 
-            for path in map(DeliveryPath, permutations(haul)):
+            for path in permutations(haul):
+                path = DeliveryPath(haul, make_mst=False)
                 if path.length < shortest_path.length:
                     shortest_path = path
 
-            if shortest_path.profit > best_path.profit:
+            if shortest_path > best_path:
                 best_path = shortest_path
 
     print(f"best profit: ${best_path.profit:.2f}")
@@ -144,4 +28,8 @@ if __name__ == '__main__':
     print("best path:")
     for d in best_path:
         print(f'\t{d}')
-    plot_path(D, best_path)
+    best_path.plot(all_deliveries=D)
+
+
+if __name__ == '__main__':
+    main(Delivery.generate(10))
